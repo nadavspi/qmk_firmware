310f3240f2432f20d8841ea20939f0b47f3f0f19
Nadav Spiegelman <me@nadav.name>
2022-10-03 12:39:51 -0400

    Add leader keys for app switching

--- users/nadavspi/leader.c
+++ users/nadavspi/leader.c
@@ -7,8 +7,32 @@ void matrix_scan_user(void) {
     leading = false;
     leader_end();
 
-    SEQ_ONE_KEY(KC_H) {
-    //   SEND_STRING(SS_LCTL(SS_LSFT(SS_LALT(SS_LGUI(XS_F)))));
+    SEQ_TWO_KEYS(KC_O, KC_F) {
+      tap_code16(HYPR(KC_F));
+    }
+
+    SEQ_TWO_KEYS(KC_O, KC_T) {
+      tap_code16(HYPR(KC_T));
+    }
+
+    SEQ_TWO_KEYS(KC_O, KC_V) {
+      tap_code16(HYPR(KC_V));
+    }
+
+    SEQ_TWO_KEYS(KC_O, KC_C) {
+      tap_code16(HYPR(KC_C));
+    }
+
+    SEQ_TWO_KEYS(KC_O, KC_O) {
+      tap_code16(HYPR(KC_O));
+    }
+
+    SEQ_TWO_KEYS(KC_O, KC_W) {
+      tap_code16(HYPR(KC_W));
+    }
+
+    SEQ_TWO_KEYS(KC_O, KC_D) {
+      tap_code16(HYPR(KC_D));
     }
   }
 }
