1b747dd5455bc7ad47c038eeecfa25916da4fef8
Nadav Spiegelman <nadav.spiegelman@adp.com>
2022-10-20 17:23:43 -0400

    Update moonlander

--- keyboards/moonlander/keymaps/nadavspi/keymap.c
+++ keyboards/moonlander/keymaps/nadavspi/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     DEFAULT_L_THUMB, _______, _______, _______, _______, DEFAULT_R_THUMB
   ),  
 
-  [1] = LAYOUT_moonlander_wrapper(
+  [_NUM] = LAYOUT_moonlander_wrapper(
     NUM_L1, _______, _______, NUM_R1,
     NUM_L2, _______, _______, NUM_R2,
     NUM_L3, _______, _______, NUM_R3,
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     NUM_L_THUMB, _______, _______, _______, _______, NUM_R_THUMB
   ),
 
-  [2] = LAYOUT_moonlander_wrapper(
+  [_FUNC] = LAYOUT_moonlander_wrapper(
     FUNC_L1, _______, _______, FUNC_R1,
     FUNC_L2, _______, _______, FUNC_R2,
     FUNC_L3, _______, _______, FUNC_R3,
@@ -42,6 +42,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      FUNC_R5, _______, _______,
 
      FUNC_L_THUMB, _______, _______, _______, _______, FUNC_R_THUMB 
+  ),
+
+  [_SYM] = LAYOUT_moonlander_wrapper(
+    SYM_L1, _______, _______, SYM_R1,
+    SYM_L2, _______, _______, SYM_R2,
+    SYM_L3, _______, _______, SYM_R3,
+    SYM_L4, SYM_R4,
+
+    _______, _______,  SYM_L5, 
+      _______, _______, // big thumb keys
+     SYM_R5, _______, _______,
+
+     SYM_L_THUMB, _______, _______, _______, _______, SYM_R_THUMB 
   ),
 
 };
