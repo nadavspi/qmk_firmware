35b4499e84a14b720bc43ce4d08ad95e2a0b2b32
Nadav Spiegelman <me@nadav.name>
2022-10-26 11:54:42 -0400

    Add ploopy

---
+++ keyboards/ploopyco/trackball/keymaps/nadav/keymap.c
@@ -0,0 +1,12 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT( 
+        KC_BTN1, DRAG_SCROLL, KC_BTN2,
+          KC_BTN1, MO(1)
+    ),
+    [1] = LAYOUT(
+        KC_BTN4, DRAG_SCROLL, KC_BTN5,
+          DPI_CONFIG, _______
+    )
+};
